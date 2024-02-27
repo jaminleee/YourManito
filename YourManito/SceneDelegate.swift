03d8693841f5a8d45ea2d46c12cf5c3b82bd3ec9
jaminleee <jamin_00@naver.com>
2024-02-27 13:39:04 -0600

    Initial Commit

---
+++ YourManito/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  YourManito
+//
+//  Created by 이자민 on 2/26/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+            
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
+    }
+
+}
+
---
+++ YourManito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourManito
+//
+//  Created by 이자민 on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
