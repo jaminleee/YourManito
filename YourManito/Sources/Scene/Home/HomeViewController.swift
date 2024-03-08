a2f20d7e95b9e50633093b0ba158d855bbdca931
jaminleee <jamin_00@naver.com>
2024-03-08 11:22:52 -0600

    [Chore] 폴더링

--- YourManito/Sources/Scene/Home/HomeViewController.swift
+++ YourManito/Sources/Scene/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  YourManito
 //
 //  Created by 이자민 on 2/26/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
