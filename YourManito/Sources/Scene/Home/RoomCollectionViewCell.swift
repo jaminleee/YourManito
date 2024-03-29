9fd0807df18a888acd86f5adddeab848e5914cec
jaminleee <jamin_00@naver.com>
2024-03-28 23:51:40 -0500

    [Chore] 컬렉션뷰 셀 생성

---
+++ YourManito/Sources/Scene/Home/RoomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RoomCollectionViewCell.swift
+//  YourManito
+//
+//  Created by 이자민 on 3/28/24.
+//
+
+import UIKit
+
+final class RoomCollectionViewCell: UICollectionViewCell {
+    static let identifier: String = "RoomCollectionViewCell"
+}
