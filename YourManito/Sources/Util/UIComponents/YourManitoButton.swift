2c615676e2d8df82dc5d56b535e0addd34644aa1
jaminleee <jamin_00@naver.com>
2024-04-08 11:29:42 -0500

    [Chore] Make UIButton Component

--- YourManito/Sources/Util/UIComponents/YourManitoButton.swift
+++ YourManito/Sources/Util/UIComponents/YourManitoButton.swift
@@ -5,4 +5,19 @@
 //  Created by 이자민 on 4/5/24.
 //
 
-import Foundation
+import UIKit
+
+class YourManitoButton: UIButton {
+    init(title: String) {
+        super.init(frame: .zero)
+        self.makeCornerRound(radius: 24)
+        self.backgroundColor = .primary
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = .font(.heading_4)
+        self.setTitleColor(.main_white, for: .normal)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
